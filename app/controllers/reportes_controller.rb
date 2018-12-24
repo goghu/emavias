fd4ef075b7047d86d7846fdd36f72d169617e5c1
Cristiam Herrera Daza <cristiamherrera@gmail.com>
2018-12-24 09:58:45 -0400

    Se puso algunas validaciones

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -10,11 +10,15 @@ class ReportesController < ApplicationController
   def por_numero
     # byebug
     if params[:busca_por] == 'numero'
-      @compra = Compra.where("numero = ?", params[:docvalor]).take
+      @compra = Compra.where("numero = ?", params[:numero]).take
     else
-      @compra = Compra.where("numero = ?", params[:docvalor]).take
+      @compra = Compra.where("docvalor = ?", params[:docvalor]).take
     end
-    @derivaciones = Derivacione.where(compra_id: @compra.id).order(id: :asc)
+    if @compra.present?
+      @derivaciones = Derivacione.where(compra_id: @compra.id).order(id: :asc)
+    else
+      @derivacione = nil      
+    end
   end
   
 end
