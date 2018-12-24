d47f42410fb6720738a5f9a5dd63a7ae389e1703
Cristiam Herrera Daza <cristiamherrera@gmail.com>
2018-12-24 09:46:27 -0400

    Se termino el reporte por numero

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -10,10 +10,11 @@ class ReportesController < ApplicationController
   def por_numero
     # byebug
     if params[:busca_por] == 'numero'
-      @compra = Compra.where("numero = ?", params[:docvalor])
+      @compra = Compra.where("numero = ?", params[:docvalor]).take
     else
-      @compra = Compra.where("numero = ?", params[:docvalor])
+      @compra = Compra.where("numero = ?", params[:docvalor]).take
     end
+    @derivaciones = Derivacione.where(compra_id: @compra.id).order(id: :asc)
   end
   
 end
--- app/models/compra.rb
+++ app/models/compra.rb
@@ -1,3 +1,4 @@
 class Compra < ApplicationRecord
   has_many :item
+  belongs_to :user, optional: true
 end
