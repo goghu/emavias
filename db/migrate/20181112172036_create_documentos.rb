6597a436a60dc834a68ce62cc781e5497a20402f
Cristiam Herrera Daza <cristiamherrera@gmail.com>
2018-12-30 21:47:55 -0400

    Se cambio el espacio del formulario

--- db/migrate/20181112172036_create_documentos.rb
+++ db/migrate/20181112172036_create_documentos.rb
@@ -1,7 +1,7 @@
 class CreateDocumentos < ActiveRecord::Migration[5.1]
   def change
     create_table :documentos do |t|
-      t.string :descripcion, :limit => 20
+      t.string :descripcion, :limit => 150
       t.integer :correlativo, :limit => 2
       t.boolean :presento
       t.string :observaciones
